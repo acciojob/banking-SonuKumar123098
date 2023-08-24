ce77e641ac615c5c73d575675bb17e877e928d25
Sonu Kumar <sonukrsingh998@gmail.com>
2023-08-24 16:45:00 +0530

    f

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -24,7 +24,7 @@ public class SavingsAccount extends BankAccount{
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-        if(amount>maxWithdrawalLimit)throw new Exception("Maximum Withdraw Limit Exceed");
+        if(amount>maxWithdrawalLimit)throw new Exception("Maximum Withdraw Limit Exceeded");
         if(getBalance()<amount) throw new Exception("Insufficient Balance");
 
         setBalance(getBalance()-amount);
